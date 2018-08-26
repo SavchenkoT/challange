d6edc00aa1ca777b2822d90801fcb5d197b52d61
Alexander Pavlov <ap@tsys.pw>
2018-08-26 20:38:45 +0300

    * Сделал так, чтобы курсы валют запрашивались после загрузки файлов с функциями  * Внес несколько незначительных исправлений, чтобы убрать ошибки в консоли

--- docs/js/btc.js
+++ docs/js/btc.js
@@ -1,28 +1,27 @@
-var requestURL = 'https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD';
+function requestAndShowBtc() {
+    var requestURL = 'https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD';
 
-var request = new XMLHttpRequest();
+    var request = new XMLHttpRequest();
 
-// Check for the various File API support.
-if (window.File && window.FileReader && window.FileList && window.Blob) {
-  // Great success! All the File APIs are supported.
-} else {
-  alert('The File APIs are not fully supported in this browser.');
-}
+    // Check for the various File API support.
+    if (window.File && window.FileReader && window.FileList && window.Blob) {
+        // Great success! All the File APIs are supported.
+    } else {
+        alert('The File APIs are not fully supported in this browser.');
+    }
 
-request.open('GET', requestURL);
+    request.open('GET', requestURL);
 
-request.responseType = 'json';
-request.send();
+    request.responseType = 'json';
+    request.send();
 
-request.onload = function() {
-  var btc = request.response;
-  populateBtc(btc);
-  showBtc(btc);
+    request.onload = function() {
+        var btc = request.response;
+        populateBtc(btc);
+    }
 }
 
 function populateBtc(jsonObj) {
   var finalBtc = document.getElementById('final-btc');
   finalBtc.textContent = jsonObj['last'];
-  getElementById('final-btc').innerText = jsonData.finalBtc
-
 }
--- docs/js/eth.js
+++ docs/js/eth.js
@@ -1,29 +1,29 @@
-var requestURL = 'https://apiv2.bitcoinaverage.com/indices/global/ticker/ETHUSD';
+function requestAndShowEth() {
+    var requestURL = 'https://apiv2.bitcoinaverage.com/indices/global/ticker/ETHUSD';
 
-var request = new XMLHttpRequest();
+    var request = new XMLHttpRequest();
 
 // Check for the various File API support.
-if (window.File && window.FileReader && window.FileList && window.Blob) {
-  // Great success! All the File APIs are supported.
-} else {
-  alert('The File APIs are not fully supported in this browser.');
-}
+    if (window.File && window.FileReader && window.FileList && window.Blob) {
+        // Great success! All the File APIs are supported.
+    } else {
+        alert('The File APIs are not fully supported in this browser.');
+    }
 
-request.open('GET', requestURL);
+    request.open('GET', requestURL);
 
-request.responseType = 'json';
-request.send();
+    request.responseType = 'json';
+    request.send();
 
-request.onload = function() {
-  var eth = request.response;
-  populateEth(eth);
-  showEth(eth);
+    request.onload = function() {
+        var eth = request.response;
+        populateEth(eth);
+    }
 }
 
 function populateEth(jsonObj) {
   var finalEth = document.getElementById('final-eth');
   finalEth.textContent = jsonObj['last'];
-  getElementById('final-eth').innerText = jsonData.finalEth
 }
 
 // var data = require('https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD');
